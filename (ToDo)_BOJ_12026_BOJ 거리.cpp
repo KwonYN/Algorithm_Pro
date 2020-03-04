ce0867926e63dfc71322a8ab470e9bac07a6e1b1
KwonYN <54017486+KwonYN@users.noreply.github.com>
2020-03-04 12:04:35 +0900

    Update (ToDo)_BOJ_12026_BOJ 거리.cpp

--- (ToDo)_BOJ_12026_BOJ 거리.cpp
+++ (ToDo)_BOJ_12026_BOJ 거리.cpp
@@ -8,7 +8,72 @@
 	스타트는 B, O, J, B, O, J, B, O, J, ... 순서로 보도블록을 밟으면서 점프를 할 것.
 	스타트가 링크를 만나는데 필요한 에너지 양의 최솟값?
 */
-#if 01
+
+
+// BOJ거리 다시
+#if 0
+#pragma warning(disable: 4996)
+#include <iostream>
+#include <string>
+#include <queue>
+#include <algorithm>
+using namespace std;
+#define NMAX	(1010)
+#define INF		(987654321)
+
+int N, min_energy;
+string block;
+int block_num[NMAX];
+int d[NMAX];
+
+void dfs_dp(int now) {
+	if (now == N - 1) return;
+
+	for (int i = now + 1; i < N; i++) {
+		if (block_num[i] == (block_num[now] + 1) % 3) {
+			if (d[i] > d[now] + (i - now) * (i - now)) {
+				d[i] = d[now] + (i - now) * (i - now);
+				dfs_dp(i);
+			}
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);			cout.tie(NULL);
+	freopen("in.txt", "r", stdin);
+	cin >> N;
+	cin >> block;
+	for (int i = 0; i < N; i++) d[i] = INF;
+	for (int i = 0; i < N; i++) {
+		if (block[i] == 'B') block_num[i] = 0;
+		else if (block[i] == 'O') block_num[i] = 1;
+		else block_num[i] = 2;
+	}
+
+	d[0] = 0;
+	dfs_dp(0);
+	if (d[N - 1] == INF) min_energy = -1;
+	else min_energy = d[N - 1];
+	cout << min_energy << "\n";
+	return 0;
+}
+#endif
+
+
+
+/*
+	BOJ에서 왜 메모리 초과가 났을까?
+	=> V(정점)의 최대 개수는 1000개
+	   그에 따라 E(간선)의 최대 개수는 1000*1001/2...
+	   다익스트라의 최악의 시간복잡도는 V^3 logV....
+	   log V는 민힙에 저장하는 시간복잡도. (빼는데는 상수 시간이 걸림!)
+	   V^3은 모든 정점 V에 대하여 모든 간선(E == V^2)에 보내야 하므로.
+	   하지만 i번 째에서는 뒤로 못 가고 앞으로만 갈 수 있음. (E가 1/2로 줄음)
+	   그래도 결국(최고 차수만 치니까) V^3 log V라서 터지게 되는 거임..!!
+*/
+#if 0
 #pragma warning(disable: 4996)
 
 #include <iostream>
@@ -34,7 +99,7 @@ int main() {
 	cin >> N;
 	cin >> block;
 	for (int i = 0; i < N; i++) van_dijk[i] = INF;
-	for (int i = 0; i < N-1; i++) {
+	for (int i = 0; i < N - 1; i++) {
 		if (block[i] == 'B') {
 			for (int j = i + 1; j < N; j++) {
 				if (block[j] == 'O') edges[i].push_back({ (j - i) * (j - i), j });
@@ -45,7 +110,7 @@ int main() {
 				if (block[j] == 'J') edges[i].push_back({ (j - i) * (j - i), j });
 			}
 		}
-		else if(block[i] == 'J') {
+		else if (block[i] == 'J') {
 			for (int j = i + 1; j < N; j++) {
 				if (block[j] == 'B') edges[i].push_back({ (j - i) * (j - i), j });
 			}
@@ -61,14 +126,14 @@ int main() {
 		pii cur = pq.top();		 pq.pop();
 		if (van_dijk[cur.second] < cur.first) continue;
 		van_dijk[cur.second] = cur.first;
-		if (cur.second == N-1) break;
+		if (cur.second == N - 1) break;
 		for (pii child : edges[cur.second]) {
 			if (van_dijk[cur.second] + child.first < van_dijk[child.second])
-				pq.push({ van_dijk[cur.second] + child.first , child.second});
+				pq.push({ van_dijk[cur.second] + child.first , child.second });
 		}
 	}
-	if (van_dijk[N-1] == INF) min_energy = -1;
-	else min_energy = van_dijk[N-1];
+	if (van_dijk[N - 1] == INF) min_energy = -1;
+	else min_energy = van_dijk[N - 1];
 
 	cout << min_energy << "\n";
 	return 0;
