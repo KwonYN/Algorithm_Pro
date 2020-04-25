d7ad1900d1bd536125441ee3dee6580f3747d4e5
KwonYN <54017486+KwonYN@users.noreply.github.com>
2020-04-25 16:58:08 +0900

    Update (ToDo_정수론)_BOJ_1837_암호제작.cpp

--- (ToDo_정수론)_BOJ_1837_암호제작.cpp
+++ (ToDo_정수론)_BOJ_1837_암호제작.cpp
@@ -1,5 +1,55 @@
 // BOJ_1837_암호제작
 #if 01
+#pragma warning(disable: 4996)
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+#define KMAX    (1000000)
+
+string P;
+int K, len, r;
+int sosu[KMAX], sosu_cnt;
+bool eratos[KMAX+10], quality;
+
+bool get_quotient(int n) {
+    int num = 0;
+    for (int i = 0; i < len; i++) {
+        num = (num * 10 + (P[i] - '0')) % n;
+    }
+    return (num == 0);
+}
+
+int main() {
+    freopen("in.txt", "r", stdin);
+    ios::sync_with_stdio(false);    cin.tie(NULL);    cout.tie(NULL);
+    eratos[0] = eratos[1] = true;      sosu_cnt = 0;
+    for (int i = 2; i <= KMAX; i++) {
+        if (eratos[i]) continue;    // eratos[i] == false인 값이
+        sosu[++sosu_cnt] = i;       // sosu[sosu_cnt]에 들어감!
+        for (int j = i + i; j <= KMAX; j += i) eratos[j] = true;
+    }
+    cin >> P;               cin >> K;       
+    len = P.length();       quality = true;
+    for (int i = 1; i <= sosu_cnt; i++) {
+        if (sosu[i] >= K) break;
+        else if (get_quotient(sosu[i])) {
+            r = sosu[i];
+            quality = false;
+            break;
+        }
+    }
+    if (quality) cout << "GOOD" << "\n";
+    else cout << "BAD " << r << "\n";
+    return 0;
+}
+#endif
+
+
+
+
+// BOJ_1837_암호제작
+#if 01
 #pragma warning(disable : 4996)
 #include <iostream>
 #include <string>
